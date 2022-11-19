c963156ffdbc1a74e6308a6b10166fd640379545
Philipp Andert <andert.philipp@gmail.com>
2022-11-19 15:15:07 +0100

    improved premake clean command

--- premake5.lua
+++ premake5.lua
@@ -23,8 +23,7 @@ project "HelloWorld"
 			print("clean the build...")
 			os.rmdir("./build")
 			os.remove("*.sln")
-   			os.remove("*.vcxproj")
-			os.remove("*.user")
+   			os.remove("*.vcxproj*")
 			print("done.")
 		end
 	}
